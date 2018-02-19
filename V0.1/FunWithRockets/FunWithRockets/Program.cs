dc7ebba4763c723eb355c868c5d3d04be2804309
AN <AN@ANDESKTOP>
2018-02-19 17:35:15 +0100

    Add more data

--- V0.1/FunWithRockets/FunWithRockets/Program.cs
+++ V0.1/FunWithRockets/FunWithRockets/Program.cs
@@ -11,9 +11,21 @@ namespace FunWithRockets
             var connection = new Connection();
             var spaceCenter = connection.SpaceCenter();
             var vessel = spaceCenter.ActiveVessel;
-            var refFrame = vessel.Orbit.Body.ReferenceFrame;
+            var obtFrame = vessel.Orbit.Body.NonRotatingReferenceFrame;
+            var srfFrame = vessel.Orbit.Body.ReferenceFrame;
+            var flight = spaceCenter.ActiveVessel.Flight();
+            vessel.Control.ActivateNextStage();
+            System.Threading.Thread.Sleep(2000);
+            vessel.Control.Throttle = 1;
             while (true)
-                Console.WriteLine(vessel.Position(refFrame));
+            {
+                Console.Clear();
+                Console.WriteLine(String.Format("Altitude above sea: {0}", Math.Round(flight.MeanAltitude, 3).ToString()));
+                Console.WriteLine(String.Format("Altitude above ground: {0}", Math.Round(flight.SurfaceAltitude, 3).ToString()));
+                Console.WriteLine(String.Format("Orbit Speed: {0}", Math.Round(vessel.Flight(obtFrame).Speed, 3).ToString()));
+                Console.WriteLine(String.Format("Surface Speed: {0}", Math.Round(vessel.Flight(srfFrame).Speed, 3).ToString()));
+                System.Threading.Thread.Sleep(500);
+            }
         }
     }
 }
