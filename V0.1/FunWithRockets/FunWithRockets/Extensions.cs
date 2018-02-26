ed290c06b7a8387b5b739406013481b7205f1666
Andreas Nicolaisen <andreasnico97@gmail.com>
2018-02-26 14:25:42 +0100

    refactor

--- V0.1/FunWithRockets/FunWithRockets/Extensions.cs
+++ V0.1/FunWithRockets/FunWithRockets/Extensions.cs
@@ -12,26 +12,29 @@ namespace FunWithRockets
             vessel.Control.Throttle = thr;
         }
 
-        public static void PrintTelemetry(this Vessel vessel)
+        public static void PrintTelemetry(this Vessel vessel, int decimals=3)
         {
+            double Round(double n)
+            {
+                return Math.Round(n, decimals);
+            }
             var flight = vessel.Flight();
             var obtFrame = vessel.Orbit.Body.NonRotatingReferenceFrame;
             var srfFrame = vessel.Orbit.Body.ReferenceFrame;
 
-            Console.WriteLine("Altitude above sea: {0}", Math.Round(flight.MeanAltitude, 3));
-            Console.WriteLine("Altitude above ground: {0}", Math.Round(flight.SurfaceAltitude, 3));
+            Console.WriteLine("Altitude above sea: {0}", Round(flight.MeanAltitude));
+            Console.WriteLine("Altitude above ground: {0}", Round(flight.SurfaceAltitude));
 
-            Console.WriteLine("Orbit Speed: {0}", Math.Round(vessel.Flight(obtFrame).Speed, 3));
-            Console.WriteLine("Surface Speed: {0}", Math.Round(vessel.Flight(srfFrame).Speed, 3));
-            Console.WriteLine("Vertical Speed: {0}",
-                Math.Round(vessel.Flight(srfFrame).VerticalSpeed, 3));
+            Console.WriteLine("Orbit Speed: {0}", Round(vessel.Flight(obtFrame).Speed));
+            Console.WriteLine("Surface Speed: {0}", Round(vessel.Flight(srfFrame).Speed));
+            Console.WriteLine("Vertical Speed: {0}", Round(vessel.Flight(srfFrame).VerticalSpeed));
 
-            Console.WriteLine("Current throttle: {0}", Math.Round(vessel.Control.Throttle, 3));
-            Console.WriteLine("Max thrust: {0}", Math.Round(vessel.AvailableThrust, 3));
-            Console.WriteLine("Current thrust: {0}", Math.Round(vessel.Thrust, 3));
+            Console.WriteLine("Current throttle: {0}", Round(vessel.Control.Throttle));
+            Console.WriteLine("Max thrust: {0}", Round(vessel.AvailableThrust));
+            Console.WriteLine("Current thrust: {0}", Round(vessel.Thrust));
 
-            Console.WriteLine("Ship mass: {0}", Math.Round(vessel.Mass, 3));
-            Console.WriteLine("Current engine acceleration: {0}", Math.Round((vessel.Thrust / vessel.Mass), 3));
+            Console.WriteLine("Ship mass: {0}", Round(vessel.Mass));
+            Console.WriteLine("Current engine acceleration: {0}", Round(vessel.Thrust / vessel.Mass));
         }
 
         public static void Land(this Vessel vessel)
@@ -60,36 +63,30 @@ namespace FunWithRockets
             while (distanceToGround > 0.5)
             {
                 double initialSpeedSquared = Math.Pow(vessel.Flight(srfFrame).Speed, 2);
-                float a = (float)(((3+initialSpeedSquared) / (2 * distanceToGround)) + 9.8);
+                float acc = (float)(((3+initialSpeedSquared) / (2 * distanceToGround)) + 9.8);
 
-                float thrustRequired = vessel.Mass * a;
+                float thrustRequired = vessel.Mass * acc;
                 float throttle = thrustRequired / (vessel.AvailableThrust);
-
-                if (vessel.Flight(srfFrame).Speed < 20)
-                {
-                    vessel.Control.SASMode = SASMode.StabilityAssist;
-                    
-                }
-                else
-                {
-                    vessel.Control.SASMode = SASMode.Retrograde;
-                }
 
                 if (throttle > 0.98) { shouldThrust = true;  }
                 if (throttle < 0.1)  { shouldThrust = false; }
 
-                if(shouldThrust) { vessel.Accelerate(a); }
+                if(shouldThrust) { vessel.Accelerate(acc); }
+                vessel.Control.SASMode =
+                    vessel.Flight(srfFrame).Speed < 20 ?
+                        SASMode.StabilityAssist : SASMode.Retrograde;
 
                 comOffset = Math.Abs(vessel.BoundingBox(vessel.ReferenceFrame).Item1.Item2);
                 distanceToGround = vessel.Flight().SurfaceAltitude - comOffset;
             }
-            while(!(vessel.Situation == VesselSituation.Landed || vessel.Situation == VesselSituation.Splashed))
+
+            while(!(vessel.Situation == VesselSituation.Landed ||
+                    vessel.Situation == VesselSituation.Splashed))
             {
-                vessel.Accelerate((float)1);
+                vessel.Accelerate(1);
                 Console.WriteLine(vessel.Flight(srfFrame).Speed);
             }
             vessel.Accelerate(0);
-            
         }
     }
 }
